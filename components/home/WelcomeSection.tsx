d2507174bc2c4a3c2118dd0dbf9c7adf5516d736
oppassky <oppasskybest@gmail.com>
2026-07-16 20:25:25 +0100

    year chenge

--- components/home/WelcomeSection.tsx
+++ components/home/WelcomeSection.tsx
@@ -91,7 +91,7 @@ export default function WelcomeSection() {
           />
         )}
         <p style={{ color: 'var(--ink-light)', fontSize: '0.85rem', marginTop: '0.75rem' }}>
-          (Website updated in April 2024)
+          (Website updated in july 2026)
         </p>
       </section>
     </>
